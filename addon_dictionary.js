74e4129d9fdf3fc0236c6e9b35d814f1cfd48dcf
Killerpommes <>
2025-05-14 18:11:29 +0200

    Add Addon Thaumic Boots

--- addon_dictionary.js
+++ addon_dictionary.js
@@ -1,51 +1,59 @@
 var addon_dictionary = {
-	"fm": {
-		"name": "Forbidden Magic",
-		"aspects":["wrath", "nether", "gluttony", "envy", "sloth", "pride", "lust"],
-		"combinations":{
-			"wrath": ["weapon", "fire"],
-			"nether": ["fire", "magic"],
-			"gluttony": ["hunger", "void"],
-			"envy": ["senses", "hunger"],
-			"sloth": ["trap", "soul"],
-			"pride": ["flight", "void"],
-			"lust": ["flesh", "hunger"]
-		} 
-	},
-	"mb": {
-		"name": "Magic Bees",
-		"aspects":["time"],
-		"combinations":{
-			"time": ["void", "order"]
-		} 
-	},
-  "gt": {
-    "name": "Gregtech",
-    "aspects": ["electricity", "magnetism", "cheatiness", "radioactivity", "stupidity"],
-    "combinations": {
-      "electricity": ["energy", "mechanism"],
-      "magnetism": ["metal", "travel"],
-      "cheatiness": ["mine", "greed"],
-      "radioactivity": ["light", "energy"],
-      "stupidity": ["entropy", "mind"]
+    "fm": {
+        "name": "Forbidden Magic",
+        "aspects": ["wrath", "nether", "gluttony", "envy", "sloth", "pride", "lust"],
+        "combinations": {
+            "wrath": ["weapon", "fire"],
+            "nether": ["fire", "magic"],
+            "gluttony": ["hunger", "void"],
+            "envy": ["senses", "hunger"],
+            "sloth": ["trap", "soul"],
+            "pride": ["flight", "void"],
+            "lust": ["flesh", "hunger"]
+        }
+    },
+    "mb": {
+        "name": "Magic Bees",
+        "aspects": ["time"],
+        "combinations": {
+            "time": ["void", "order"]
+        }
+    },
+    "gt": {
+        "name": "Gregtech",
+        "aspects": ["electricity", "magnetism", "cheatiness", "radioactivity", "stupidity"],
+        "combinations": {
+            "electricity": ["energy", "mechanism"],
+            "magnetism": ["metal", "travel"],
+            "cheatiness": ["mine", "greed"],
+            "radioactivity": ["light", "energy"],
+            "stupidity": ["entropy", "mind"]
+        }
+    },
+    "av": {
+        "name": "Avaritia",
+        "aspects": ["apocalypse"],
+        "combinations": {
+            "apocalypse": ["greed", "eldritch"]
+        }
+    },
+    "gtnh": {
+        "name": "GTNH (2.1.3.0+)",
+        "aspects": ["equality", "madness", "beginning", "constellation", "glory"],
+        "combinations": {
+            "equality": ["mind", "order"],
+            "madness": ["mind", "taint"],
+            "beginning": ["void", "motion"],
+            "constellation": ["light", "beginning"],
+            "glory": ["man", "travel"]
+        }
+    },
+    "tb": {
+        "name": "Thaumic Boots",
+        "aspects": ["space", "boots"],
+        "combinations": {
+            "space": ["crystal", "metal"],
+            "boots": ["armor", "travel"]
+        }
     }
-  },
-  "av": {
-	  "name": "Avaritia",
-	  "aspects": ["apocalypse"],
-	  "combinations": {
-		  "apocalypse": ["greed", "eldritch"]
-	  }
-  },
-  "gtnh": {
-	  "name": "GTNH (2.1.3.0+)",
-	  "aspects": ["equality", "madness", "beginning", "constellation", "glory"],
-	  "combinations": {
-		  "equality": ["mind", "order"],
-		  "madness": ["mind", "taint"],
-		  "beginning": ["void", "motion"],
-		  "constellation": ["light", "beginning"],
-		  "glory": ["man", "travel"]
-	  }
-  }
 };
